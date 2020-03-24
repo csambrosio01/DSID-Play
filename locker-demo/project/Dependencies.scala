cdfe0eb0f800d2ec45943c264766aa575796f2d0
Caio <ambrosio.caio13@gmail.com>
2020-03-23 22:10:33 -0300

    Add play slick, pĺay slick evolutions and postgres configuration to project

--- locker-demo/project/Dependencies.scala
+++ locker-demo/project/Dependencies.scala
@@ -1,8 +1,14 @@
+import sbt._
 import play.sbt.PlayImport._
 
 object Dependencies {
 
+  private val playSlick = "com.typesafe.play" %% "play-slick" % "5.0.0"
+  private val playSlickEvolutions = "com.typesafe.play" %% "play-slick-evolutions" % "5.0.0"
+
+  private val postgres = "org.postgresql" % "postgresql" % "42.2.11"
+
   val dependencies = Seq(
-    guice
+    playSlick, playSlickEvolutions, postgres, guice
   )
 }
