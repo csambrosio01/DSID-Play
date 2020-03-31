6ec8c10c75fd415d068bfa4752cf8914eed84780
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-03-31 09:55:22 -0300

    Delete index method in home controller

--- locker-demo/app/controllers/HomeController.scala
+++ locker-demo/app/controllers/HomeController.scala
@@ -4,16 +4,4 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(userAction: UserInfoAction, controllerComponents: ControllerComponents) extends AbstractController(controllerComponents) {
-
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index(): Action[AnyContent] = userAction { implicit request: UserRequest[_] =>
-    Ok(views.html.index(form))
-  }
-}
+class HomeController @Inject()(userAction: UserInfoAction, controllerComponents: ControllerComponents) extends AbstractController(controllerComponents) {}
