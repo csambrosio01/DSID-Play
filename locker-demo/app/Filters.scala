36df8ec35bdc3b555d0d0396a0c911d001982607
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-03-31 11:57:06 -0300

    Add class Filters on root, to customize what filters should be available on application

---
+++ locker-demo/app/Filters.scala
@@ -0,0 +1,11 @@
+import filter.LoggingFilter
+import javax.inject.Inject
+import play.api.http.{DefaultHttpFilters, EnabledFilters}
+import play.filters.gzip.GzipFilter
+
+class Filters @Inject()(
+                          defaultFilters: EnabledFilters,
+                          gzip: GzipFilter,
+                          log: LoggingFilter
+                        )
+  extends DefaultHttpFilters(defaultFilters.filters :+ gzip :+ log: _*)
