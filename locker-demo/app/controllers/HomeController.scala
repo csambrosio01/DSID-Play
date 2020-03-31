ec9370525774730bd0cbd2d67e978eebb5bd2fc9
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-03-31 09:56:02 -0300

    Add method to consult session on HomeController

--- locker-demo/app/controllers/HomeController.scala
+++ locker-demo/app/controllers/HomeController.scala
@@ -1,7 +1,20 @@
 package controllers
 
 import javax.inject._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 @Singleton
-class HomeController @Inject()(userAction: UserInfoAction, controllerComponents: ControllerComponents) extends AbstractController(controllerComponents) {}
+class HomeController @Inject()(userAction: UserInfoAction, controllerComponents: ControllerComponents) extends AbstractController(controllerComponents) {
+
+  def consultSession: Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+    val cookies = request.cookies.map( cookie => (cookie.name, cookie.value, cookie.path, cookie.domain))
+
+    val json = Json.obj(
+      "cookie" ->  Json.toJson(cookies),
+      "session" -> request.session.get(SESSION_ID)
+    )
+
+    Ok(json)
+  }
+}
