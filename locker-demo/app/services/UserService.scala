9d8e20559118566a13da0b213be3c8eda0e9eaab
Caio Ambrosio <ambrosio.caio13@gmail.com>
2020-03-31 11:54:23 -0300

    Add method login on user service

--- locker-demo/app/services/UserService.scala
+++ locker-demo/app/services/UserService.scala
@@ -1,16 +1,34 @@
 package services
 
+import exception.WrongCredentialsException
 import javax.inject.Inject
-import model.User
+import model.{Login, User}
 import persistance.user.SqlUserRepository
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class UserService @Inject()(
                              userRepository: SqlUserRepository
+                           )
+                           (
+                             implicit ec: ExecutionContext
                            ) {
 
   def createUser(user: User): Future[User] = {
     userRepository.create(user)
+  }
+
+  def login(login: Login): Future[User] = {
+    userRepository
+      .findUserByUsername(login.username)
+      .map { user =>
+        user.fold(throw WrongCredentialsException("Wrong credentials")) { user =>
+          if (user.password == login.password) {
+            user
+          } else {
+            throw WrongCredentialsException("Wrong credentials")
+          }
+        }
+      }
   }
 }
